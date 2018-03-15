c5260dbb369b8c5048dc673439e1c21e8a330e61
Jeff Dickey <jdickey@seven-sigma.com>
2018-03-16 00:47:31 +0800

    Fix tagging issues and release 0.11.2
    * Corrected ordering of name tokens in Debian 'slim' images. [Fixes #4]
    * Fixed some minor errors in tagging.
    * Added, then commented out, building older Ruby versions on Debian Stretch. (See Issue #5.)

--- README.md
+++ README.md
@@ -3,7 +3,9 @@
 # Contents
 
 - [Overview](#overview)
-  * [Supported Tags and Respective Dockerfile Links](#supported-tags-and-respective-dockerfile-links)
+  * [Supported Tags](#supported-tags)
+    + [Logical but Nonexistent Tags](#logical-but-nonexistent-tags)
+    + [What? No Dockerfiles?](#what-no-dockerfiles)
 - [Software](#software)
   * [Debian Stretch or Jessie](#debian-stretch-or-jessie)
     + [Base Software](#base-software)
@@ -12,6 +14,14 @@
   * [Alpine Linux](#alpine-linux)
     + [Base Software](#base-software-1)
     + [Qt5 Software](#qt5-software)
+- [Changelog](#changelog)
+  * [0.11.2 (15 March 2018)](#0112-15-march-2018)
+  * [0.11.1 (7 March 2018)](#0111-7-march-2018)
+  * [0.11.0 (4 March 2018)](#0110-4-march-2018)
+  * [0.10.0 (1 March 2018)](#0100-1-march-2018)
+  * [0.9.0 (11 January 2018)](#090-11-january-2018)
+  * [0.8.0 (8 January 2018)](#080-8-january-2018)
+  * [0.7.0 (17 November 2017)](#070-17-november-2017)
 - [Additional Documentation](#additional-documentation)
 - [Legal](#legal)
 
@@ -19,28 +29,41 @@
 
 I *often* build from Ruby [official base images](https://hub.docker.com/_/ruby/), install additional software packages, and do some basic Ruby housekeeping (installing Bundler and making sure the system Gems are up-to-date). Building against [`2.4.2-jessie`](https://github.com/docker-library/ruby/blob/73d3ed6b06738a7457a24fba9024cad303829c0a/2.4/jessie/Dockerfile) on a 2011 iMac and a decent Net connection, this can take about *20 minutes.* Repeat this half-a-dozen times over the course of a day and you've lost two hours. As Orwell wrote, *doubleplus ungood.*
 
-## Supported Tags and Respective Dockerfile Links
+## Supported Tags
 
-* `2.5.0-stretch`, `2.5-stretch`, `2-stretch`, `stretch`, `2.5.0`, `2.5`, `2`, `latest` ([*2.5.0/stretch/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.5.0/stretch/Dockerfile))
-* `2.5.0-stretch-no-qt`, `2.5-stretch-no-qt`, `2-stretch-no-qt`, `stretch-no-qt`, `2.5.0-no-qt`, `2.5-no-qt`, `2-no-qt` ([*2.5.0/stretch/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.5.0/stretch/no-qt/Dockerfile))
-* `2.5.0-slim-stretch`, `2.5-slim-stretch`, `2-slim-stretch`, `slim-stretch`, `2.5.0-slim`, `2.5-slim` ([*2.5.0/stretch/slim/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.5.0/stretch/slim/Dockerfile))
-* `2.5.0-slim-stretch-no-qt`, `2.5-slim-stretch-no-qt`, `2-slim-stretch-no-qt`, `slim-stretch-no-qt`, `2.5.0-slim-no-qt`, `2.5-slim-no-qt`, `2-slim-no-qt`, `slim-no-qt` ([*2.5.0/stretch/slim/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.5.0/stretch/slim/no-qt/Dockerfile))
-* `2.5.0-alpine`, `2.5-alpine`, `2-alpine`, `alpine` ([*2.5.0/stretch/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.5.0/stretch/Dockerfile))
-* `2.5.0-alpine-no-qt`, `2.5-alpine-no-qt`, `2-alpine-no-qt`, `alpine-no-qt` ([*2.5.0/alpine/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.5.0/alpine/no-qt/Dockerfile))
-* `2.4.3-jessie`, `2.4-jessie`, `2.4.3`, `2.4` ([*2.4.3/jessie/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.3/jessie/Dockerfile))
-* `2.4.3-jessie-no-qt`, `2.4-jessie-no-qt`, `2.4.3-no-qt`, `2.4-no-qt` ([*2.4.3/jessie/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.3/jessie/no-qt/Dockerfile))
-* `2.4.3-slim-jessie`, `2.4-slim-jessie`, `2.4.3-slim`, `2.4-slim` ([*2.4.3/jessie/slim/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.3/jessie/slim/Dockerfile))
-* `2.4.3-slim-jessie-no-qt`, `2.4-slim-jessie-no-qt`, `2.4.3-slim-no-qt`, `2.4-slim-no-qt` ([*2.4.3/jessie/slim/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.3/jessie/slim/no-qt/Dockerfile))
-* `2.4.2-jessie`, `2.4.2` ([*2.4.2/jessie/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.2/jessie/Dockerfile))
-* `2.4.2-jessie-no-qt`, `2.4.2-no-qt` ([*2.4.2/jessie/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.2/jessie/no-qt/Dockerfile))
-* `2.4.2-slim-jessie`, `2.4.2-slim` ([*2.4.2/jessie/slim/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4.3/jessie/slim/Dockerfile))
-* `2.4.2-slim-jessie-no-qt`, `2.4.2-slim-no-qt` ([*2.4.2/jessie/slim/no-qt/Dockerfile*](https://github.com/jdickey/docker-ruby/blob/master/2.4/jessie/slim/no-qt/Dockerfile))
+Each image has one tag that follows the format `2.x.y-os_build[-no-qt]`, where
 
-The `*-no-qt` tags have no version of the Qt GUI libraries installed; those not so marked have Qt5 for (Debian) Stretch and Alpine, and Qt4 for (Debian) Jessie.
+1. `2.x.y` is the full version number of the Ruby version hosted by the image, which will be one of `2.5.0` (the current version), `2.4.3`, or `2.4.2`;
+2. `os_build` identifies which OS and variant the image was based on. These can be any one of
+	1. `jessie`: Debian [Jessie](https://en.wikipedia.org/wiki/Debian#Code_names) (8.0);
+	2. `slim-jessie`: A "slim" version of Jessie, containing fewer packages by default (and thus with a considerably smaller image size);
+	3. `stretch`: Debian [Stretch](https://en.wikipedia.org/wiki/Debian#Code_names) (9.0);
+	4. `stretch-slim`: A "slim" version of Stretch;
+	5. `alpine3.7` (synonyms: `alpine37` and `alpine`): Alpine Linux 3.7, a minimalist Linux distribution. (There is no 'slim' version of `alpine`; it's already the smallest of the listed images);
+3. The suffix `-no-qt` indicates that the image has been built *without* the Qt OS-level libraries and tools needed to run the [Capybara](https://teamcapybara.github.io/capybara/) test framework (and thus does not include Capybara itself or the `capybara-webkit` headless browser).
 
-There are no "slim" images for Alpine, as the entire point of Alpine is *to be a* minimal base distribution.
+The `latest` tag identifies the latest version of Ruby (as of December 2017, version 2.5.0) on the latest, non-`slim` version of Debian (currently `stretch`), built *with* the Qt and Capybara tools. It should be the default choice when you simply want the most recent supported Ruby version, but is not recommended for production use in most cases. (We recommend exploring basing your image on an `alpine` and `-no-qt` build for production.)
 
-Why no tags for, e.g., `2-slim-jessie`? We use tag names intended to be consistent with those of the [official Ruby Docker images](https://hub.docker.com/_/ruby/); the unadorned `2` states that the tag is for the latest 2.*x* Ruby version. All Debian Jessie-based images are for the outdated 2.4.*x* (as of early March 2018, 2.4.3) release. It is possible that some such tags were created in error and not caught prior to release on Docker Hub; if you find one, please [open an issue](https://github.com/jdickey/docker-ruby/issues). *Thanks!*
+Minor-version tags such as `2.4-jessie` or `2.5-alpine-no-qt` identify the latest supported release of that minor version of Ruby on the specified OS build. As later versions of Ruby are released and supported (e.g., a hypothetical 2.4.4 or 2.5.1), the corresponding minor-version tag will be redefined to match the new full-version-number tag.
+
+Major-version tags (e.g., `2-stretch` or `2-alpine3.7`) identify the latest supported minor release of the latest supported major release of Ruby (currently `2.5.0`), and are updated in a manner analogous to minor-version tags; i.e., when Ruby 2.6 is released (expected to be in December, 2018), each major-version tag will be updated to match `2.6.0` on each respective operating system (e.g., `2.6.0-stretch`).
+
+Tags which *do not* include a Ruby version number are built using the latest supported version of Ruby on the OS build identified by their name, e.g., `stretch-no-qt` or `alpine`, built with Qt and Capybara. This differs from `latest` in that `latest` is presently defined as always being on the latest non-`slim` Debian build (currently `stretch`).
+
+Finally, the Alpine Linux OS build names differ from the Debian conventions in that Alpine is the only OS build that encodes (major and minor) version numbers. Hence, `alpine3.7` is synonymous with `alpine37` and, until a newer version of Alpine is supported by the [official Ruby base images](https://hub.docker.com/_/ruby/), `alpine` is as well. That is to say that `2.5.0-alpine3.7` and `alpine` (presently) reference the same image.
+
+### Logical but Nonexistent Tags
+
+Why no tags for, e.g., `2-slim-jessie`? We use tag names intended to be consistent with those of the [official Ruby Docker images](https://hub.docker.com/_/ruby/); the unadorned `2` states that the tag is for the latest 2.*x* Ruby version. No Debian Jessie-based images exist for Ruby versions later than the outdated 2.4.*x* (as of early March 2018, 2.4.3) release. It is possible that some such tags were created in error and not caught prior to release on Docker Hub; if you find one, please [open an issue](https://github.com/jdickey/docker-ruby/issues). *Thanks!*
+
+### What? No Dockerfiles?
+
+Versions of these images prior to [Release 0.11.0](#changelog) published `Dockerfile`s for each supported Ruby version/OS build variant. Release 0.11.0 and later use [Docker build arguments](https://docs.docker.com/engine/reference/commandline/build/#set-build-time-variables---build-arg) to configure the appropriate "template" `Dockerfiles` instead, depending on whether a Debian or Alpine image is being built, with or without Qt and Capybara. These templates are:
+
+* [`Dockerfile.main.alpine`](https://github.com/jdickey/docker-ruby/blob/master/Dockerfile.main.alpine);
+* [`Dockerfile.main.debian`](https://github.com/jdickey/docker-ruby/blob/master/Dockerfile.main.debian);
+* [`Dockerfile.no-qt.alpine`](https://github.com/jdickey/docker-ruby/blob/master/Dockerfile.no-qt.alpine); and
+* [`Dockerfile.no-qt.debian`](https://github.com/jdickey/docker-ruby/blob/master/Dockerfile.no-qt.debian).
 
 # Software
 
@@ -99,6 +122,12 @@ The following Alpine packages are installed in Alpine images not tagged `no-qt` 
 * `xvfb`
 
 # Changelog
+
+## 0.11.2 (15 March 2018)
+
+* Corrected ordering of name tokens in Debian 'slim' images. (Fixes Issue #4.)
+* Fixed some minor errors in tagging.
+* Added, then commented out, building older Ruby versions on Debian Stretch. (See Issue #5.)
 
 ## 0.11.1 (7 March 2018)
 
--- build-all.sh
+++ build-all.sh
@@ -1,15 +1,58 @@
 #!/bin/bash
+set -euo pipefail
+#
+# Notes on tag naming (copied to 'verify_images' as well):
+#
+# 1. Each image **should** have one fully-qualified tag; i.e., it contains the
+#    full Ruby version number included in the image; identifies the OS it was
+#    built with (currently Debian 'stretch' or 'jessie', or 'alpine-3.7'); *and*
+#    whether or not it contains Qt/Capybara support (where '-no-qt` indicates
+#    that it does not, but see Rule 3).
+# 2. The fully-qualified tag mentioned in Rule 1 **may optionally** be the tag
+#    initially assigned to the image when it was built.
+# 3. An image tag *not* containing either the '-qt' or '-no-qt' indicators
+#    **must** be interpreted as the otherwise-equivalent '-qt' image. The
+#    explicit tagging of that image with an explicit '-qt' tag is *optional*.
+# 4. An image tag without an OS identifier *must be* built on the official
+#    Debian OS used for the upstream image: 'jessie' for 2.4.x and earlier, or
+#    'stretch' for 2.5.0 and later.
+# 5. An image tag containing a _partial_ version number, such as '2-alpine'
+#    or '2.4-jessie-no-qt', **must** be synonymous with the _latest_ matching
+#    version at the time the image was built. As of March 2018, for the examples
+#    given, those would be '2.5.0-alpine-qt' and '2.4.3-jessie-no-qt',
+#    respectively.
+# 6. An image tag containing *no* Ruby version number *must* match the _latest
+#    version of Ruby packaged in the upstream images_ at the time these images
+#    were built and tagged.
+# 7. If an OS is no longer supported for a given Ruby version upstream (e.g.,
+#    'jessie' for Ruby 2.5.0 and later), then any image built on that OS
+#    **must** include a Ruby version number for which that OS is supported. This
+#    **may** be a partial version number; e.g., the tag '2.4-jessie-no-qt' is a
+#    valid alias for (presently) '2.4.3-jessie-no-qt'. However, '2-jessie-no-qt'
+#    and 'jessie-no-qt' **are not** valid tags, as the current Version 2.x
+#    release (and latest overall) of Ruby is currently 2.5.0.
+#
+# Further note: Ruby 2.4.2 and 2.4.3 on Stretch from the official Ruby images
+# fail to build the extensions for Capybara. A day and a half of poking at the
+# problem yielded jdickey/docker-ruby#5.
+#
 
-export VERSION=0.11.1
+# if [[ -z $DOCKER_RUBY_VERSION ]]; then
+#   echo You MUST define the DOCKER_RUBY_VERSION environment variable when using this script.
+#   echo It will be encoded into the built images as a version ID.
+#   echo Example usage: DOCKER_RUBY_VERSION=0.27.0 bash ./build_all.sh
+#   return 1
+# fi
+VERSION=$DOCKER_RUBY_VERSION
 ###
 ### 2.5.0 on Debian Stretch
 ###
 
 docker build --build-arg RUBY_VERSION=2.5.0 \
-             --build-arg RUBY_EXTRA='-stretch' \
-             --build-arg VERSION=$VERSION \
-             --tag 'jdickey/ruby:2.5.0-stretch-no-qt' \
-             --squash --file Dockerfile.no-qt.debian .
+            --build-arg RUBY_EXTRA='-stretch' \
+            --build-arg VERSION=$VERSION \
+            --tag 'jdickey/ruby:2.5.0-stretch-no-qt' \
+            --squash --file Dockerfile.no-qt.debian .
 
 docker build --build-arg RUBY_VERSION=2.5.0 \
              --build-arg RUBY_EXTRA='-stretch' \
@@ -31,37 +74,6 @@ docker build --build-arg RUBY_VERSION=2.5.0 \
              --tag 'jdickey/ruby:2.5.0-slim-stretch' \
              --squash --file Dockerfile.main.debian .
 
-docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:2.5.0
-docker tag jdickey/ruby:2.5.0 jdickey/ruby:latest
-docker tag jdickey/ruby:2.5.0 jdickey/ruby:2
-docker tag jdickey/ruby:2.5.0 jdickey/ruby:2.5
-docker tag jdickey/ruby:2.5.0 jdickey/ruby:stretch
-docker tag jdickey/ruby:2.5.0 jdickey/ruby:2-stretch
-docker tag jdickey/ruby:2.5.0 jdickey/ruby:2.5-stretch
-
-docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:2.5.0-no-qt
-docker tag jdickey/ruby:2.5.0-no-qt jdickey/ruby:2-no-qt
-docker tag jdickey/ruby:2.5.0-no-qt jdickey/ruby:2.5-no-qt
-docker tag jdickey/ruby:2.5.0-no-qt jdickey/ruby:2-stretch-no-qt
-docker tag jdickey/ruby:2.5.0-no-qt jdickey/ruby:2.5-stretch-no-qt
-docker tag jdickey/ruby:2.5.0-no-qt jdickey/ruby:stretch-no-qt
-
-docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:2.5.0-slim-no-qt
-docker tag jdickey/ruby:2.5.0-slim-no-qt jdickey/ruby:slim-no-qt
-docker tag jdickey/ruby:2.5.0-slim-no-qt jdickey/ruby:2-slim-no-qt
-docker tag jdickey/ruby:2.5.0-slim-no-qt jdickey/ruby:2.5-slim-no-qt
-docker tag jdickey/ruby:2.5.0-slim-no-qt jdickey/ruby:2-slim-stretch-no-qt
-docker tag jdickey/ruby:2.5.0-slim-no-qt jdickey/ruby:2.5-slim-stretch-no-qt
-docker tag jdickey/ruby:2.5.0-slim-no-qt jdickey/ruby:slim-stretch-no-qt
-
-docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:2.5.0-slim
-docker tag jdickey/ruby:2.5.0-slim jdickey/ruby:slim
-docker tag jdickey/ruby:2.5.0-slim jdickey/ruby:2-slim
-docker tag jdickey/ruby:2.5.0-slim jdickey/ruby:2.5-slim
-docker tag jdickey/ruby:2.5.0-slim jdickey/ruby:2-slim-stretch
-docker tag jdickey/ruby:2.5.0-slim jdickey/ruby:2.5-slim-stretch
-docker tag jdickey/ruby:2.5.0-slim jdickey/ruby:slim-stretch
-
 ###
 ### 2.5.0 on Alpine 3.7
 ###
@@ -79,17 +91,35 @@ docker build --build-arg RUBY_VERSION=2.5.0 \
              --tag 'jdickey/ruby:2.5.0-alpine3.7' \
              --squash --file Dockerfile.main.alpine .
 
-docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5.0-alpine37-no-qt
-docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5.0-alpine-no-qt
-docker tag jdickey/ruby:2.5.0-alpine-no-qt jdickey/ruby:2.5-alpine-no-qt
-docker tag jdickey/ruby:2.5.0-alpine-no-qt jdickey/ruby:2-alpine-no-qt
-docker tag jdickey/ruby:2.5.0-alpine-no-qt jdickey/ruby:alpine-no-qt
+###
+### 2.4.3 on Debian Stretch — disabled per jdickey/docker-ruby Issue #5.
+###
 
-docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:jdickey/ruby:2.5.0-alpine37
-docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2.5.0-alpine
-docker tag jdickey/ruby:2.5.0-alpine jdickey/ruby:2.5-alpine
-docker tag jdickey/ruby:2.5.0-alpine jdickey/ruby:2-alpine
-docker tag jdickey/ruby:2.5.0-alpine jdickey/ruby:alpine
+# docker build --build-arg RUBY_VERSION=2.4.3 \
+#              --build-arg RUBY_EXTRA='-stretch' \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.3-stretch-no-qt' \
+#              --squash --file Dockerfile.no-qt.debian .
+
+# docker build --build-arg RUBY_VERSION=2.4.3 \
+#              --build-arg RUBY_EXTRA='-stretch' \
+#              --build-arg QTLIBS='libqt5webkit5 libqt4-dev qt4-qmake' \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.3-stretch' \
+#              --squash --file Dockerfile.main.debian .
+
+# docker build --build-arg RUBY_VERSION=2.4.3 \
+#              --build-arg RUBY_EXTRA='-slim-stretch' \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.3-slim-stretch-no-qt' \
+#              --squash --file Dockerfile.no-qt.debian .
+
+# docker build --build-arg RUBY_VERSION=2.4.3 \
+#              --build-arg RUBY_EXTRA='-slim-stretch' \
+#              --build-arg QTLIBS="libqt4-webkit libqt4-dev qt4-qmake" \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.3-slim-stretch' \
+#              --squash --file Dockerfile.main.debian .
 
 ###
 ### 2.4.3 on Debian Jessie
@@ -118,39 +148,38 @@ docker build --build-arg RUBY_VERSION=2.4.3 \
              --build-arg RUBY_EXTRA='-slim-jessie' \
              --build-arg QTLIBS="libqt4-webkit libqt4-dev qt4-qmake" \
              --build-arg VERSION=0.11.0 \
-             --tag 'jdickey/ruby:2.4.3-jessie-slim' \
+             --tag 'jdickey/ruby:2.4.3-slim-jessie' \
              --squash --file Dockerfile.main.debian .
 
-docker tag jdickey/ruby:2.4.3-jessie jdickey/ruby:2.4.3
-docker tag jdickey/ruby:2.4.3 jdickey/ruby:latest
-docker tag jdickey/ruby:2.4.3 jdickey/ruby:2
-docker tag jdickey/ruby:2.4.3 jdickey/ruby:2.5
-docker tag jdickey/ruby:2.4.3 jdickey/ruby:jessie
-docker tag jdickey/ruby:2.4.3 jdickey/ruby:2-jessie
-docker tag jdickey/ruby:2.4.3 jdickey/ruby:2.5-jessie
+###
+### 2.4.2 on Debian Stretch
+###
 
-docker tag jdickey/ruby:2.4.3-jessie-no-qt jdickey/ruby:2.4.3-no-qt
-docker tag jdickey/ruby:2.4.3-no-qt jdickey/ruby:2-no-qt
-docker tag jdickey/ruby:2.4.3-no-qt jdickey/ruby:2.5-no-qt
-docker tag jdickey/ruby:2.4.3-no-qt jdickey/ruby:2-jessie-no-qt
-docker tag jdickey/ruby:2.4.3-no-qt jdickey/ruby:2.5-jessie-no-qt
-docker tag jdickey/ruby:2.4.3-no-qt jdickey/ruby:jessie-no-qt
+# docker build --build-arg RUBY_VERSION=2.4.2 \
+#              --build-arg RUBY_EXTRA='-stretch' \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.2-stretch-no-qt' \
+#              --squash --file Dockerfile.no-qt.debian .
 
-docker tag jdickey/ruby:2.4.3-slim-jessie-no-qt jdickey/ruby:2.4.3-slim-no-qt
-docker tag jdickey/ruby:2.4.3-slim-no-qt jdickey/ruby:slim-no-qt
-docker tag jdickey/ruby:2.4.3-slim-no-qt jdickey/ruby:2-slim-no-qt
-docker tag jdickey/ruby:2.4.3-slim-no-qt jdickey/ruby:2.5-slim-no-qt
-docker tag jdickey/ruby:2.4.3-slim-no-qt jdickey/ruby:2-slim-jessie-no-qt
-docker tag jdickey/ruby:2.4.3-slim-no-qt jdickey/ruby:2.5-slim-jessie-no-qt
-docker tag jdickey/ruby:2.4.3-slim-no-qt jdickey/ruby:slim-jessie-no-qt
+# docker build --build-arg RUBY_VERSION=2.4.2 \
+#              --build-arg RUBY_EXTRA='-stretch' \
+#              --build-arg QTLIBS="libqt4-webkit libqt4-dev qt4-qmake" \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.2-stretch' \
+#              --squash --file Dockerfile.main.debian .
 
-docker tag jdickey/ruby:2.4.3-slim-jessie jdickey/ruby:2.4.3-slim
-docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:slim
-docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2-slim
-docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2.5-slim
-docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2-slim-jessie
-docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2.5-slim-jessie
-docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:slim-jessie
+# docker build --build-arg RUBY_VERSION=2.4.2 \
+#              --build-arg RUBY_EXTRA='-slim-stretch' \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.2-slim-stretch-no-qt' \
+#              --squash --file Dockerfile.no-qt.debian .
+
+# docker build --build-arg RUBY_VERSION=2.4.2 \
+#              --build-arg RUBY_EXTRA='-slim-stretch' \
+#              --build-arg QTLIBS="libqt4-webkit libqt4-dev qt4-qmake" \
+#              --build-arg VERSION=0.11.0 \
+#              --tag 'jdickey/ruby:2.4.2-slim-stretch' \
+#              --squash --file Dockerfile.main.debian .
 
 ###
 ### 2.4.2 on Debian Jessie
@@ -182,36 +211,104 @@ docker build --build-arg RUBY_VERSION=2.4.2 \
              --tag 'jdickey/ruby:2.4.2-slim-jessie' \
              --squash --file Dockerfile.main.debian .
 
-docker tag jdickey/ruby:2.4.2-jessie jdickey/ruby:2.4.2
-docker tag jdickey/ruby:2.4.2 jdickey/ruby:latest
-docker tag jdickey/ruby:2.4.2 jdickey/ruby:2
-docker tag jdickey/ruby:2.4.2 jdickey/ruby:2.5
-docker tag jdickey/ruby:2.4.2 jdickey/ruby:jessie
-docker tag jdickey/ruby:2.4.2 jdickey/ruby:2-jessie
-docker tag jdickey/ruby:2.4.2 jdickey/ruby:2.5-jessie
+###
+### Version tags
+###
+
+docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:2.5-stretch-no-qt
+docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:2-stretch-no-qt
+docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:stretch-no-qt
+docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:2.5.0-no-qt
+docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:2.5-no-qt
+
+docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:2.5-stretch
+docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:2-stretch
+docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:stretch
+docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:2.5.0
+docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:2.5
+
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:2.5-slim-stretch-no-qt
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:2-slim-stretch-no-qt
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:slim-stretch-no-qt
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:2.5.0-slim-no-qt
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:2.5-slim-no-qt
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:2-slim-no-qt
+docker tag jdickey/ruby:2.5.0-slim-stretch-no-qt jdickey/ruby:slim-no-qt
+
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:2.5-slim-stretch
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:2-slim-stretch
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:slim-stretch
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:2.5.0-slim
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:2.5-slim
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:2-slim
+docker tag jdickey/ruby:2.5.0-slim-stretch jdickey/ruby:slim
+
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5.0-alpine37-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5.0-alpine-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5-alpine3.7-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5-alpine37-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2.5-alpine-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2-alpine3.7-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2-alpine37-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:2-alpine-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:alpine3.7-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:alpine37-no-qt
+docker tag jdickey/ruby:2.5.0-alpine3.7-no-qt jdickey/ruby:alpine-no-qt
+
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2.5.0-alpine37
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2.5.0-alpine
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2.5-alpine3.7
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2.5-alpine37
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2.5-alpine
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2-alpine3.7
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2-alpine37
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:2-alpine
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:alpine3.7
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:alpine37
+docker tag jdickey/ruby:2.5.0-alpine3.7 jdickey/ruby:alpine
+
+docker tag jdickey/ruby:2.4.3-jessie-no-qt jdickey/ruby:2.4-jessie-no-qt
+docker tag jdickey/ruby:2.4.3-jessie-no-qt jdickey/ruby:2.4.3-no-qt
+docker tag jdickey/ruby:2.4.3-jessie-no-qt jdickey/ruby:2.4-no-qt
+
+docker tag jdickey/ruby:2.4.3-jessie jdickey/ruby:2.4.3
+docker tag jdickey/ruby:2.4.3 jdickey/ruby:2.4
+docker tag jdickey/ruby:2.4.3 jdickey/ruby:jessie
+
+docker tag jdickey/ruby:2.4.3-slim-jessie-no-qt jdickey/ruby:2.4.3-slim-no-qt
+docker tag jdickey/ruby:2.4.3-slim-jessie-no-qt jdickey/ruby:2.4-slim-jessie-no-qt
+docker tag jdickey/ruby:2.4.3-slim-jessie-no-qt jdickey/ruby:2.4-slim-no-qt
+
+docker tag jdickey/ruby:2.4.3-slim-jessie jdickey/ruby:2.4.3-slim
+docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2.4-slim
+docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2-slim-jessie
+docker tag jdickey/ruby:2.4.3-slim jdickey/ruby:2.4-slim-jessie
+
+docker tag jdickey/ruby:2.4.3-no-qt jdickey/ruby:2.4-jessie-no-qt
+
+# Don't tag 2.4.3-jessie as 'jessie'; that implies "current version of Ruby on
+# Jessie", which this isn't. (There is no 2.5.0 on Jessie.)
+docker tag jdickey/ruby:2.4.3 jdickey/ruby:2.4-jessie
+
+docker tag jdickey/ruby:2.4.3-slim-jessie-no-qt jdickey/ruby:2.4-slim-jessie-no-qt
+
+docker tag jdickey/ruby:2.4.3-slim-jessie jdickey/ruby:2.4-slim-jessie
 
 docker tag jdickey/ruby:2.4.2-jessie-no-qt jdickey/ruby:2.4.2-no-qt
-docker tag jdickey/ruby:2.4.2-no-qt jdickey/ruby:2-no-qt
-docker tag jdickey/ruby:2.4.2-no-qt jdickey/ruby:2.5-no-qt
-docker tag jdickey/ruby:2.4.2-no-qt jdickey/ruby:2-jessie-no-qt
-docker tag jdickey/ruby:2.4.2-no-qt jdickey/ruby:2.5-jessie-no-qt
-docker tag jdickey/ruby:2.4.2-no-qt jdickey/ruby:jessie-no-qt
 
-docker tag jdickey/ruby:2.4.2-slim-jessie-no-qt jdickey/ruby:2.4.2-slim-no-qt
-docker tag jdickey/ruby:2.4.2-slim-no-qt jdickey/ruby:slim-no-qt
-docker tag jdickey/ruby:2.4.2-slim-no-qt jdickey/ruby:2-slim-no-qt
-docker tag jdickey/ruby:2.4.2-slim-no-qt jdickey/ruby:2.5-slim-no-qt
-docker tag jdickey/ruby:2.4.2-slim-no-qt jdickey/ruby:2-slim-jessie-no-qt
-docker tag jdickey/ruby:2.4.2-slim-no-qt jdickey/ruby:2.5-slim-jessie-no-qt
-docker tag jdickey/ruby:2.4.2-slim-no-qt jdickey/ruby:slim-jessie-no-qt
+docker tag jdickey/ruby:2.4.2-jessie jdickey/ruby:2.4.2
 
 docker tag jdickey/ruby:2.4.2-slim-jessie jdickey/ruby:2.4.2-slim
-docker tag jdickey/ruby:2.4.2-slim jdickey/ruby:slim
-docker tag jdickey/ruby:2.4.2-slim jdickey/ruby:2-slim
-docker tag jdickey/ruby:2.4.2-slim jdickey/ruby:2.5-slim
-docker tag jdickey/ruby:2.4.2-slim jdickey/ruby:2-slim-jessie
-docker tag jdickey/ruby:2.4.2-slim jdickey/ruby:2.5-slim-jessie
-docker tag jdickey/ruby:2.4.2-slim jdickey/ruby:slim-jessie
+
+###
+### Defaults
+###
+
+docker tag jdickey/ruby:2.5.0-stretch-no-qt jdickey/ruby:2-no-qt
+docker tag jdickey/ruby:2-no-qt jdickey/ruby:no-qt
+
+docker tag jdickey/ruby:2.5.0-stretch jdickey/ruby:2
+docker tag jdickey/ruby:2 jdickey/ruby:latest
 
 ###
 ### Fin
